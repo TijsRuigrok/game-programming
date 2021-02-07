a244e5556486f5265fbc09bec68b6c6bd2add99b
TijsRuigrok <TijsRuigrok15@gmail.com>
2021-02-07 12:47:07 +0100

    Week 1 of Game Programming homework finished.

---
+++ Week_1/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float sendDogCooldown = 2f;
+    bool sendDogActive = false;
+    float timePassedSinceDogSend;
+    float timePassed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassedSinceDogSend = Time.time - timePassed;
+        if (timePassedSinceDogSend > sendDogCooldown)
+        {
+            sendDogActive = false;
+        }
+
+        if (!sendDogActive)
+        {
+            SendDog();
+            timePassed = Time.time;
+        }  
+    }
+
+    private void SendDog()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            sendDogActive = true;
+        }
+    }
+}
